474750d4ce05a3d82e3593be6d2fc41dc4c226b3
bhomik-glitch <awebsite601@gmail.com>
2025-07-14 11:04:59 +0530

    Remove unused backend folder from project

--- backend/models/Cart.js
+++
@@ -1,8 +0,0 @@
-import mongoose from 'mongoose';
-
-const cartSchema = new mongoose.Schema({
-  userId: String,
-  products: [{ productId: String, quantity: Number }]
-});
-
-export default mongoose.model('Cart', cartSchema);
--- backend/routes/orderRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { createOrder, verifyPayment } from '../controllers/orderController.js';
-
-const router = express.Router();
-router.post('/create', createOrder);
-router.post('/verify', verifyPayment);
-
-export default router;
